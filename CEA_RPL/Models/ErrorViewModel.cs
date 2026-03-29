6f984813b0368fa5b7fa1151ab5aefc144b2cc93
sa298 <sa298@SHAHZAD>
2026-03-29 11:24:26 +0530

    Add project files.

---
+++ CEA_RPL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CEA_RPL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
